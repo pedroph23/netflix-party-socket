c2dbbc496b86901421bebfa6d5544e64c7df9ba2
pedroph23 <30670185+pedroph23@users.noreply.github.com>
2020-03-26 02:49:16 -0300

    Update socket.js

--- src/socket.js
+++ src/socket.js
@@ -10,7 +10,7 @@ res.send('server is running');
 
 //SocketIO vem aqui
 
-http.listen(80, function(){
+http.listen(443, function(){
     io.on("connection", function (client) {
         console.log("Connected");
         client.on("join", function(name){
