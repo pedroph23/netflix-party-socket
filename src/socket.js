b6cfda48b5b1acbd011f5d412131d77abddceae4
pedroph23 <30670185+pedroph23@users.noreply.github.com>
2020-03-26 02:55:03 -0300

    Add files via upload

--- src/socket.js
+++ src/socket.js
@@ -10,7 +10,7 @@ res.send('server is running');
 
 //SocketIO vem aqui
 
-http.listen(443, function(){
+http.listen(8080, function(){
     io.on("connection", function (client) {
         console.log("Connected");
         client.on("join", function(name){
